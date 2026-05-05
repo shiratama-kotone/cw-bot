1faf183f3d3c7240c5d5e7c24ca0d9dc3cdcf4df
ゆゆゆ <yuyuyu1226.m.1226@gmail.com>
2026-05-05 11:33:19 +0900

    server.js の更新

--- server.js
+++ server.js
@@ -506,13 +506,13 @@ class ChatworkBotUtils {
     if (Math.random() < specialChance) return specialFortune;
 
     const fortunes = [
-      { name: '大吉', weight: 1 },
-      { name: '中吉', weight: 5 },
-      { name: '吉',   weight: 9 },
-      { name: '小吉', weight: 15 },
-      { name: '末吉', weight: 20 },
-      { name: '凶',   weight: 20 },
-      { name: '大凶', weight: 30 }
+      { name: '大吉', weight: 0.1 },
+      { name: '中吉', weight: 0.4 },
+      { name: '吉',   weight: 0.5 },
+      { name: '小吉', weight: 1 },
+      { name: '末吉', weight: 1 },
+      { name: '凶',   weight: 2 },
+      { name: '大凶', weight: 95 }
     ];
 
     const total = fortunes.reduce((sum, f) => sum + f.weight, 0);
